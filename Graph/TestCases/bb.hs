e09122c9fc12357c0a2214298d6d74637a556bc6
Prashant Singh <prash471@gmail.com>
2014-05-17 16:57:38 +0530

    Update bb.hs

--- Graph/TestCases/bb.hs
+++ Graph/TestCases/bb.hs
@@ -3,8 +3,7 @@ module Main where
 import System.Cmd
 import Data.Graph.Inductive
 import Data.Graph.Inductive.Graphviz
-import Data.Graph.Inductive.Example
- 
+
 myGraph :: Gr String ()
 --myGraph = buildG (1,6)[(1,2),(1,3),(1,4),(1,5),(1,6),(2,3),(2,4),(2,5),(2,6),(3,4),(3,5),(3,6),(4,5),(4,6),(5,6)]
 myGraph = mkGraph
